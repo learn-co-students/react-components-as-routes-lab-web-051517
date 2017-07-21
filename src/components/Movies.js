16c6a85be1288bea7be1407ef71582516e999b4e
Lili Ouaknin <aurelie.ouaknin@gmail.com>
2017-07-21 16:04:05 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,15 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+        <h1>Movies Page</h1>
+        {movies.map(movie => (
+          <div>
+            <p>Title: {movie.title}</p>
+            <p>Time: {movie.time}</p>
+            <p>Metascore: {movie.metascore}</p>
+            <ul>Genres: {movie.genres.map(genre => <li>{genre}</li>)}</ul>
+          </div>
+        ))}
     </div>
   );
 };
