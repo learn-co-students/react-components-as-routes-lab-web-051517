a6c8fe079bb2c6393b84510a972ab160124b12b4
wkhearn <wkhearn@gmail.com>
2017-07-21 13:47:07 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,9 +4,21 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div className='movie'>
+          <h3>{movie.title}</h3>
+          <p>{movie.time} minutes</p>
+            <p>Genres:</p>
+              <ul>
+                {movie.genres.map(genre => (
+                  <li>{genre}</li>
+                ))}
+              </ul>
+        </div>
+      ))}
     </div>
-  );
-};
+  )
+}
 
-export default Movies;
+export default Movies
