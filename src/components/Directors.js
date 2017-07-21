eef6db167b6cdeb54b3c231bbf79a89629819e08
Zach Dubno <zachedubno@gmail.com>
2017-07-21 13:26:55 -0400

    Done.

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -4,7 +4,13 @@ import { directors } from '../data';
 const Directors = () => {
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {directors.map(d=>(
+        <div>
+          {d.name}
+          <ul>{d.movies.map(i=>(<li>{i}</li>))}</ul>
+        </div>
+      ))}
     </div>
   );
 }
