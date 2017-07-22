e184a53a67f40543e97726b32d0ea781b72ed247
github username <github email address>
2017-07-22 17:20:37 -0400

    Done.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { 
-  BrowserRouter as Router, 
-  Route
+import {
+  BrowserRouter as Router,
+  Route, NavLink
 } from 'react-router-dom';
 import NavBar from '../components/NavBar';
 import Home from '../components/Home';
@@ -13,7 +13,16 @@ import Movies from '../components/Movies';
 const App = (props) => {
   return (
     <Router>
-      {/*{code here}*/}
+      <div>
+      <NavBar />
+      <br/>
+      <br/>
+      <br/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/actors" component={Actors} />
+      <Route exact path="/directors" component={Directors} />
+      <Route exact path="/movies" component={Movies} />
+      </div>
     </Router>
   );
 };
